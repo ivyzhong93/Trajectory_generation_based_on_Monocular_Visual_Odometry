f74a2195149c3a31756c72906a5d298a23683f9f
zhezhong625 <450620568@qq.com>
2016-12-10 03:15:20 -0500

    1

---
+++ My_MonoVO_iOS4Tests.m
@@ -0,0 +1,39 @@
+//
+//  My_MonoVO_iOS4Tests.m
+//  My_MonoVO_iOS4Tests
+//
+//  Created by Peter on 12/7/16.
+//  Copyright © 2016 Carnegie Mellon University. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface My_MonoVO_iOS4Tests : XCTestCase
+
+@end
+
+@implementation My_MonoVO_iOS4Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
